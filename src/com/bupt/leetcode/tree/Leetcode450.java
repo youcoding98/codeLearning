5f6325f8949b97964d44c43d615f7ac3e95d3c7d
youcoding98 <geyou0217@163.com>
2020-08-19 10:45:28 +0800

    Leetcode450:删除二叉搜索树中的节点

---
+++ src/com/bupt/leetcode/tree/Leetcode450.java
@@ -0,0 +1,36 @@
+package com.bupt.leetcode.tree;
+
+/**
+ * Leetcode450:删除二叉搜索树中的节点
+ * @author Administrator
+ */
+public class Leetcode450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null){
+            return null;
+        }
+        if (root.val == key){
+            if (root.left == null){
+                return root.right;
+            }
+            if (root.right == null){
+                return root.left;
+            }
+            TreeNode minNode = getMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right,minNode.val);
+        }else if (root.val > key){
+            root.left = deleteNode(root.left, key);
+        }else if(root.val < key){
+            root.right = deleteNode(root.right,key);
+        }
+        return root;
+    }
+
+    public TreeNode getMin(TreeNode root){
+        while (root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+}
