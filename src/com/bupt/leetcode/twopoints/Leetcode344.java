f059bd30562d96fcc0fb50e856bb3bc0a689e3c5
youcoding98 <geyou0217@163.com>
2020-09-19 19:52:03 +0800

    Leetcode344:反转字符串

---
+++ src/com/bupt/leetcode/twopoints/Leetcode344.java
@@ -0,0 +1,19 @@
+package com.bupt.leetcode.twopoints;
+
+/**
+ * Leetcode344:反转字符串
+ * @author Administrator
+ */
+public class Leetcode344 {
+    public void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length - 1;
+        while (i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
