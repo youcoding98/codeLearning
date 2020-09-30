fdaf26994f0ba834153c68318ad0ebad14c00927
youcoding98 <geyou0217@163.com>
2020-09-30 14:50:48 +0800

    Leetcode024:两两交换链表中的节点

---
+++ src/com/bupt/leetcode/list/Leetcode024.java
@@ -0,0 +1,25 @@
+package com.bupt.leetcode.list;
+
+/**
+ * Leetcode024:两两交换链表中的节点
+ * @author Administrator
+ */
+public class Leetcode024 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode pre = newHead;
+        while (head != null && head.next != null){
+            ListNode p = head;
+            ListNode q = head.next;
+
+            pre.next = q;
+            p.next = q.next;
+            q.next = p;
+
+            pre = p;
+            head = p.next;
+        }
+        return newHead.next;
+    }
+}
