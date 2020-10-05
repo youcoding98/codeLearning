04c0fd82fea177edb16d57091f51aa2341b3be04
youcoding98 <geyou0217@163.com>
2020-10-05 19:37:06 +0800

    Leetcode567:字符串的排列

---
+++ src/com/bupt/leetcode/slidingwindows/Leetcode567.java
@@ -0,0 +1,42 @@
+package com.bupt.leetcode.slidingwindows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Leetcode567:字符串的排列
+ * @author Administrator
+ */
+public class Leetcode567 {
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1map = new int[26];
+        int[] s2map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+            s2map[s2.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1map, s2map))
+                return true;
+            s2map[s2.charAt(i + s1.length()) - 'a']++;
+            s2map[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1map, s2map);
+    }
+    public static boolean matches(int[] s1map, int[] s2map) {
+        for (int i = 0; i < 26; i++) {
+            if (s1map[i] != s2map[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+        System.out.println(checkInclusion(s1,s2));
+    }
+}
