838d6dfb8b0368fc803517e322ed1f48b1bf37f7
youcoding98 <geyou0217@163.com>
2020-10-16 09:52:22 +0800

    1016每日一题打卡

---
+++ src/com/bupt/leetcode/dp/Leetcode978.java
@@ -0,0 +1,11 @@
+package com.bupt.leetcode.dp;
+
+/**
+ * Leetcode978:最长湍流子数组
+ * @author Administrator
+ */
+public class Leetcode978 {
+    /*public int maxTurbulenceSize(int[] A) {
+
+    }*/
+}
---
+++ src/com/bupt/leetcode/slidingwindows/Leetcode1208.java
@@ -0,0 +1,8 @@
+package com.bupt.leetcode.slidingwindows;
+
+/**
+ * Leetcode1208:尽可能使字符串相等
+ * @author Administrator
+ */
+public class Leetcode1208 {
+}
