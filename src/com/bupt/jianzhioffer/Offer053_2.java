4a54b1b057d9b8dae3d402c8311fa9f1de73bec8
youcoding98 <geyou0217@163.com>
2020-11-05 21:23:40 +0800

    Offer053_2:0~n-1中缺失的数字

---
+++ src/com/bupt/jianzhioffer/Offer053_2.java
@@ -0,0 +1,20 @@
+package com.bupt.jianzhioffer;
+
+/**
+ * Offer053_2:0~n-1中缺失的数字
+ * @author Administrator
+ */
+public class Offer053_2 {
+    public int missingNumber(int[] nums) {
+        int left = 0,right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > mid){
+                right = mid - 1;
+            }else if (nums[mid] == mid){
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
