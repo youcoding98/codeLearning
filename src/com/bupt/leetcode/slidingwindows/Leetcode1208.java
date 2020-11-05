9bd4e35e8b6e5652230b9656a27374d02e7ff52c
youcoding98 <geyou0217@163.com>
2020-11-05 22:25:55 +0800

    Leetcode1208:尽可能使字符串相等

--- src/com/bupt/leetcode/slidingwindows/Leetcode1208.java
+++ src/com/bupt/leetcode/slidingwindows/Leetcode1208.java
@@ -5,4 +5,25 @@ package com.bupt.leetcode.slidingwindows;
  * @author Administrator
  */
 public class Leetcode1208 {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int len = s.length();
+        int[] cost = new int[len];
+        for (int i = 0; i < len; i++) {
+            cost[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        int left = 0,right = 0;
+        int maxLen = 0;
+        int currentCost = 0;
+        while (right < len && left < len){
+            currentCost += cost[right];
+            while (currentCost > maxCost && left < len){
+                currentCost -= cost[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen,right - left + 1);
+            right++;
+
+        }
+        return maxLen;
+    }
 }
