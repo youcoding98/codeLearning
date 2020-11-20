7c4979c054ac96cfad30aacd15e8a1ddf4fdee25
“e <geyou0217@bupt.edu.cn>
2020-11-20 11:42:17 +0800

    Offer022:链表中倒数第K个节点

---
+++ src/com/bupt/jianzhioffer/Offer022.java
@@ -0,0 +1,29 @@
+package com.bupt.jianzhioffer;
+
+/**
+ * Offer022:链表中倒数第K个节点
+ * @author Administrator
+ */
+public class Offer022 {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        if (k == 0){
+            return null;
+        }
+        if (head == null){
+            return null;
+        }
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode p = newHead;
+        ListNode q = newHead;
+        while (k > 0){
+            q = q.next;
+            k--;
+        }
+        while (q != null){
+            q = q.next;
+            p = p.next;
+        }
+        return p;
+    }
+}
