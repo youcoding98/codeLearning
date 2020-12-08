036fdd2e7d50ab218a9b45c4741465cc48702c9f
“e <geyou0217@bupt.edu.cn>
2020-12-08 21:28:02 +0800

    Offer010_1:斐波那契数列

--- src/com/bupt/jianzhioffer/Offer010_1.java
+++ src/com/bupt/jianzhioffer/Offer010_1.java
@@ -13,7 +13,7 @@ public class Offer010_1 {
         dp[0] = 0;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
             dp[i] %= 1000000007;
         }
         return dp[n];
