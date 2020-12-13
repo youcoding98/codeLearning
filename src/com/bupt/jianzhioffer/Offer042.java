825714808c19e2810dad8bd5c3a54c52fd1439e1
youcoding98 <geyou0217@163.com>
2020-12-13 20:40:05 +0800

    Offer042：连续子数组的最大和

--- src/com/bupt/jianzhioffer/Offer042.java
+++ src/com/bupt/jianzhioffer/Offer042.java
@@ -5,17 +5,27 @@ package com.bupt.jianzhioffer;
  * @author Administrator
  */
 public class Offer042 {
-    public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0){
-            return 0;
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 1){
+            return nums[0];
         }
-        int[] dp = new int[nums.length + 1];
+        int len = nums.length;
+        int[] dp = new int[len];
         dp[0] = nums[0];
         int max = dp[0];
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
-            max = Math.max(max,dp[i]);
+            if (nums[i] + dp[i-1] > nums[i]){
+                dp[i] = nums[i] + dp[i-1];
+            }else {
+                dp[i] = nums[i];
+            }
+            max = Math.max(dp[i],max);
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        maxSubArray(nums);
     }
 }
