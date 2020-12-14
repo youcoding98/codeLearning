0aa0eb02617d9032738b613e5ad14af2a4a3b7a9
“e <geyou0217@bupt.edu.cn>
2020-12-14 16:15:27 +0800

    Offer047:礼物的最大价值

--- src/com/bupt/jianzhioffer/Offer047.java
+++ src/com/bupt/jianzhioffer/Offer047.java
@@ -6,21 +6,25 @@ package com.bupt.jianzhioffer;
  */
 public class Offer047 {
     public int maxValue(int[][] grid) {
-        int row = grid.length;
-        int col = grid[0].length;
-        int[][] dp = new int[row][col];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < row; i++) {
-            dp[i][0] += dp[i-1][0];
-        }
-        for (int i = 1; i < col; i++) {
-            dp[0][i] += dp[0][i-1];
-        }
-        for (int i = 1; i < row; i++) {
-            for (int j = 1; j < col; j++) {
-                dp[i][j] += Math.max(dp[i-1][j],dp[i][j-1]);
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dp[i][j] = grid[i][j];
             }
         }
-        return dp[row-1][col-1];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] += dp[i-1][0];
+        }
+        for (int i = 1; i < cols; i++) {
+            dp[0][i] += dp[0][i-1];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] += Math.max(dp[i-1][j] ,dp[i][j - 1]);
+            }
+        }
+        return dp[rows - 1][cols - 1];
     }
 }
