3ad82659afd699e6576ace91aabebc28007793cd
“e <geyou0217@bupt.edu.cn>
2020-12-16 02:09:38 +0800

    Offer53_1:在排序数组中查找数字

--- src/com/bupt/jianzhioffer/Offer053_1.java
+++ src/com/bupt/jianzhioffer/Offer053_1.java
@@ -1,33 +1,33 @@
 package com.bupt.jianzhioffer;
 
 /**
- * Offer53_1:在排序数组中查找数字、
+ * Offer53_1:在排序数组中查找数字
  * @author Administrator
  */
 public class Offer053_1 {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0){
+        if (nums.length == 0 || nums == null){
             return 0;
         }
-        int targetIndex = findTarget(target,0,nums.length-1,nums);
-        if (targetIndex == -1){
+        int index = findTarget(nums, target);
+        if (index == -1){
             return 0;
         }
         int count = 1;
-        int left = targetIndex - 1;
-        int right = targetIndex + 1;
+        int left = index - 1;
+        int right = index + 1;
         while (left >= 0){
             if (nums[left] == target){
-                count++;
                 left--;
-            }else{
+                count++;
+            }else {
                 break;
             }
         }
         while (right < nums.length){
             if (nums[right] == target){
-                count++;
                 right++;
+                count++;
             }else {
                 break;
             }
@@ -35,13 +35,14 @@ public class Offer053_1 {
         return count;
     }
 
-    public int findTarget(int target,int left,int right,int[] nums){
+    public int findTarget(int[] nums,int target){
+        int left = 0,right = nums.length - 1;
         while (left <= right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > target){
-                right = mid - 1;
-            }else if (nums[mid] < target){
+            int mid = left + (right - left)/2;
+            if (nums[mid] < target){
                 left = mid + 1;
+            }else if (nums[mid] > target){
+                right = mid - 1;
             }else if (nums[mid] == target){
                 return mid;
             }
